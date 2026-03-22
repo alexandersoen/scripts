140b9f205353eb573a1bd1b3591e94b579a80b72
alexandersoen <alexandersoen@gmail.com>
2026-03-22 23:36:32 +0100

    Update script to set the WM_CLASS properly

--- chrome_comm_tabs.sh
+++ chrome_comm_tabs.sh
@@ -10,27 +10,20 @@ URLS=(
   "https://mattermost.cmlab.dev"
 )
 MARKER="gmail"
-COM_CLASS="WM_COMM_CHROME"
+COM_INSTANCE="wm_comm_chrome"
 
-WID=$(xdotool search --classname "$COM_CLASS" | head -n 1)
+WID=$(xdotool search --classname "$COM_INSTANCE" | head -n 1)
 
 if [ -z "$WID" ]; then
   google-chrome-stable --new-window "${URLS[@]}" &
-  
+
   while [ -z "$WID" ]; do
     sleep 0.1
     # Using "gmail" / marker to find correct windows
     WID=$(xdotool search --name "$MARKER" | head -n 1)
   done
 
-  xprop -id "$WID" -f WM_CLASS 8s -set WM_CLASS "$COM_CLASS"
-
-  until xprop -id "$WID" WM_STATE 2>/dev/null | grep -q "window state: Normal"; do
-    sleep 0.1
-  done
-
-  xdotool windowactivate "$WID"
-  xdotool key "super+shift+5"
+  xdotool set_window --classname "$COM_INSTANCE" "$WID"
 fi
 
 xdotool key "super+5"
